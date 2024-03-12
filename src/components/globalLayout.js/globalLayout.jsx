58f9cef34834712dba86266ac9c7d585518e3f06
Mateus Santos <mateuspele2015@gmail.com>
2024-03-12 08:21:35 -0300

    create new menu

--- src/components/globalLayout.js/globalLayout.jsx
+++ src/components/globalLayout.js/globalLayout.jsx
@@ -1,40 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react';
+import './index.css';
 import {
   ArrowDownOutlined,
-  CustomerServiceOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
-  UserAddOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import SendIcon from "@mui/icons-material/Send";
-import { Layout, Menu, Button, theme, Input, Grid, Avatar, Tooltip, FloatButton } from 'antd';
+import { Layout, Menu, Button, theme, Grid, Avatar, FloatButton, Tooltip } from 'antd';
 import { Contexto } from '../../context/Contexto';
 import axios from 'axios';
+import { ArrowLeft, ChatSharp, Logout, Send } from '@mui/icons-material';
 import TextArea from 'antd/es/input/TextArea';
-import './index.css';
-import { ArrowBack, ArrowBackIos, ArrowBackOutlined, ArrowCircleDown, Chat, ChatSharp, Logout, LogoutOutlined, ScreenLockRotationTwoTone, ShowChart, SimCardDownload } from '@mui/icons-material';
-
-
-
-
 const { Header, Sider, Content } = Layout;
-const GlobalLayout = ({ children }) => {
-  const { user, logout, setUser, showScrow, setShowScrow } = useContext(Contexto);
+const App = ({ children }) => {
+  const [collapsedWidth, setCollapsedWidth] = useState(80);
+  const { user, logout, showScrow, setShowScrow } = useContext(Contexto);
   const screens = Grid.useBreakpoint();
   const [collapsed, setCollapsed] = useState(false);
-
-
-  useEffect(() => {
-    if (!screens.lg) {
-      setCollapsed(true);
-    } else {
-      setCollapsed(false);
-    }
-  }, [screens]);
-
   const [mensage, setMensage] = useState("")
   const {
     token: { colorBgContainer, borderRadiusLG },
@@ -70,84 +54,73 @@ const GlobalLayout = ({ children }) => {
       sendMensage();
     }
   };
+  useEffect(() => {
+    setCollapsedWidth(80 - collapsedWidth);
+    setCollapsed(false);
+    if (!screens.lg || !screens.sm || !screens.md) {
+      setCollapsed(true);
+      setCollapsedWidth(80 - collapsedWidth);
+    }
+    else {
+      setCollapsed(false);
+      setCollapsedWidth(80 - collapsedWidth);
+    }
+  }, [screens]);
 
   return (
     <Layout>
-      {screens.lg && (
-        <Sider
-          className='h-screen custom-sider'
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-          collapsedWidth={0}
-        >
-          <div className="demo-logo-vertical" />
-          <Menu
-            theme="light"
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            className='bg-gray-50  h-full'
-            items={[
-              {
-                key: '1',
-                icon: <ChatSharp style={{ height: 24, width: 24, color: "#2D6CEA" }} />,
-                label: <p className='text-xl'>Chatdata - 4</p>,
-              },
-              {
-                key: '2',
-                icon: <Avatar style={{ left: -5 }} size={35} src={"https://3.bp.blogspot.com/-F4MfDKjSuvk/Ti5iBgpMMfI/AAAAAAAAABs/txgMqwk5v3c/s1600/352308683.jpg"} />,
-                label: <p className='text-lg text-black'>Mateus santos</p>,
-                className: "bg-gray-100 mt-50",
-                style: { marginTop: 20 }
-              },
-              {
-                key: '3',
-                icon: <Logout style={{ color: "red", height: 24, width: 24 }} />,
-                label: <p className='text-lg text-[red] font-bold'>Sair</p>,
-                onClick: () => { logout() },
-                style: { marginTop: "74vh", backgroundColor: "transparent" }
-              },
-            ]}
-          />
-        </Sider>
-      )}
-
-      <Layout className='min-h-[95vh] md:min-h-[98vh]'>
-
+      <Sider className='min-h-screen '
+        breakpoint='lg' collapsedWidth={collapsedWidth} trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          className='min-h-screen'
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <ChatSharp style={{ height: 24, width: 24, color: "#2D6CEA" }} />,
+              label: <p className='text-xl'>Chatdata</p>,
+            },
+            {
+              key: '2',
+              icon: <Avatar style={{ left: -5 }} size={35} src={"https://3.bp.blogspot.com/-F4MfDKjSuvk/Ti5iBgpMMfI/AAAAAAAAABs/txgMqwk5v3c/s1600/352308683.jpg"} />,
+              label: <p className='text-lg text-black'>Conversa </p>,
+              className: "bg-gray-100 mt-50",
+              style: { marginTop: 20 }
+            },
+            {
+              key: '3',
+              icon: <Logout style={{ color: "red", height: 24, width: 24 }} />,
+              label: <p className='text-lg text-[red] font-bold'>Sair</p>,
+              onClick: () => { logout() },
+              style: { marginTop: "74vh", backgroundColor: "transparent" }
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className=' ' > {"-"}
+        {/*"Aqui é a cor do fundo em volta do filho do menu"*/}
         <Header
+
+          className=''
           style={{
             padding: 0,
-            background: "white",
+            background: colorBgContainer,
+            margin: '-22px -16px 0 0px',
           }}
         >
-
-          <div>
-            <Button
-              type="text"
-              className='hidden sm:flex'
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={() => {
-                setCollapsed(!collapsed)
-
-              }}
-              style={{
-                fontSize: '16px',
-                width: 100,
-                height: 64,
-              }}
-            />
-            <div className=' sm:hidden flex flex-nowrap px-5 pt-3 flex-row items-center justify-between'>
-              <div onClick={() => logout()} className='flex items-center gap-0  px-1 rounded-lg '>
-                <LogoutOutlined style={{ height: 40, width: 40, color: "red" }} className='ml-0 p-1' />
-                <p className='tex-white text-lg'>Sair</p>
-              </div>
-
-              <p className='tex-white text-lg   px-1 py-2  shadow-sm' >Chatdata <ChatSharp style={{ height: 24, width: 24, color: "#2D6CEA" }} /></p>
-
-
-            </div>
-          </div>
-
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
         </Header>
         <Content
           className={`max-h-[100%] h-2  p-0 overflow-y-auto  overflow-x-hidden`}
@@ -155,9 +128,8 @@ const GlobalLayout = ({ children }) => {
           {children}
 
         </Content>
-        <div className='flex justify-center items-center'>
+        <div className='flex  w-1/2justify-center items-center'>
           <form onSubmit={sendMensage} className='w-11/12 flex  gap-4 flex-nowrap justify-center itens-center'>
-
             <TextArea onClick={() => setShowScrow(true)} onChange={(e) => setMensage(e.target.value)}
               value={mensage}
               className="mb-5 w-11-12 pt-3 p-3 flex flex-col  rounded-3xl shadow-2xl border-y-2 border-gray-600 bg-gray-50  max-h-s w-full sm:w-[60%] lg:w-[50%]"
@@ -165,18 +137,26 @@ const GlobalLayout = ({ children }) => {
               onKeyUp={handleKeyPress}
               rows={1} placeholder="DIGITE SUA MENSAGEM" maxLength={5000} />
 
-            <Tooltip color={`${!showScrow ? "blue" : "red"}`} title={`${!showScrow ? "Scroll ativado" : "Scroll desativado"}`}>
-          
-            <FloatButton
-            
-              shape="circle"
-              
-              type="primary"
-              style={{ right: 94 }}
-              icon={<ArrowDownOutlined onClick={() => setShowScrow(!showScrow)} className={`   ${!showScrow ? "text-gray-50" : "text-red-300"} cursor-pointer`} />}
-            />
-            </Tooltip>
-           
+            <button className=' flex  justify-center mt-3 h-12 w-8'>
+              <Send style={{color:"blue", height:"20px"}}  className='"text-red-300'/>
+            </button>
+
+            <div className='hidden md:block'>
+
+              <Tooltip color={`${!showScrow ? "blue" : "red"}`} title={`${!showScrow ? "Scroll ativado" : "Scroll desativado"}`}>
+
+                <FloatButton
+
+                  shape="circle"
+
+                  type="primary"
+                  style={{ right: 94 }}
+                  icon={<ArrowDownOutlined onClick={() => setShowScrow(!showScrow)} className={`   ${!showScrow ? "text-gray-50" : "text-red-300"} cursor-pointer`} />}
+                />
+              </Tooltip>
+
+            </div>
+
 
           </form>
         </div>
@@ -187,4 +167,4 @@ const GlobalLayout = ({ children }) => {
     </Layout>
   );
 };
-export default GlobalLayout;
+export default App;
