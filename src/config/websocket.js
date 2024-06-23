cc63d655c9265f5b3500fa83ec06c5c044e49097
Mateus Santos <mateuspele2015@gmail.com>
2024-06-23 19:35:50 -0300

    fix: baseurl

--- src/config/websocket.js
+++ src/config/websocket.js
@@ -1,2 +1,2 @@
-//export const websocket =  'wss://present-gabbey-mateusdata-fefbff92.koyeb.app'
-export const websocket =  'ws://192.168.10.108:3001'
+export const websocket =  'wss://present-gabbey-mateusdata-fefbff92.koyeb.app'
+//export const websocket =  'ws://192.168.10.108:3001'
