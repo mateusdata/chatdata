7126cc719cd337fdde14b54ad7334495e7647998
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2024-09-05 04:48:20 +0000

    fix: darkmode in login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -80,7 +80,7 @@ function LoginForm() {
   }
 
   return (
-    <body className="dark:bg-slate-900 bg-gray-100 flex h-full items-center py-16">
+    <body className=" bg-gray-100 flex min-h-screen items-center py-16">
       <main className="w-full max-w-md mx-auto p-6">
         <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
           <div className="p-4 sm:p-7">
